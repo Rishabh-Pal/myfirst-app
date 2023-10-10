bb7aafc54dc9d2a6e2da14ed250cc70a20025280
Rishabh@iv5394 <boletorishabh1999@gmail.com>
2023-10-10 16:00:34 +0530

    first one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { FormComponent } from './server/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
