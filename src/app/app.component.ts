0505f48c8df13be46055deee5543289e3480b97f
Rishabh@iv5394 <boletorishabh1999@gmail.com>
2023-10-23 13:25:07 +0530

    add some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { UsersService } from './users.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myfirst-app';
+  constructor(private userService: UsersService) {}
+  ngOnInit() {
+    this.userService.getAllData().subscribe((data) => {
+      console.log(data);
+    });
+  }
+  
+  submitData(value: any) {
+    let body = {
+      name: value.name,
+      age: value.age,
+    };
+    console.log(value)
+    this.userService.postData(body).subscribe((response) => {
+      console.log(response);
+    });
+  }
+  updateData(value: any) {
+    let body = {
+      name: value.name,
+      age: value.age,
+    };
+    console.log(body)
+    this.userService.updateData(body,"652677dafb97dc5ce628e706").subscribe((response) => {
+        console.log(response)
+      })
+  }
+  deleteData(){
+    this.userService.deleteData("652668c75dfa5c1d28a29969").subscribe(response => {
+      console.log(response)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { FormComponent } from './server/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
